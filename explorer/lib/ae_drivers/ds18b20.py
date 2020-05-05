224ddd89f8c7110d52b4343bb286713dcc02783a
TJ <t_taiwo@live.co.uk>
2020-05-05 08:59:44 +0100

    Debugging bs18b20 driver

--- explorer/lib/ae_drivers/ds18b20.py
+++ explorer/lib/ae_drivers/ds18b20.py
@@ -13,11 +13,13 @@ from . import _AE_Peripheral_Base
 import time
 import os
 import glob
+
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
 
 class AE_DS18B20(_AE_Peripheral_Base):
     """This class is to define a DS18B20 type (water)temperature sensor.
@@ -25,28 +27,26 @@ class AE_DS18B20(_AE_Peripheral_Base):
 
     def __init__(self, name, description):
         super().__init__(name, description, 'DS18B20')
-        
-    def _read_temp_raw():
+
+    def _read_temp_raw(self):
         f = open(device_file, 'r')
         lines = f.readlines()
+        print('Lines were read in _read_temp_raw() func')
         f.close()
         return lines
 
     def _read_temp(self):
-        lines = _read_temp_raw()
+        lines = self._read_temp_raw()
+        print(f'lines are {lines}')
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
-            lines = _read_temp_raw()
+            lines = self._read_temp_raw()
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
-            temp_string = lines[1][equals_pos+2:]
+            temp_string = lines[1][equals_pos + 2:]
             temp_c = float(temp_string) / 1000.0
-            #temp_f = temp_c * 9.0 / 5.0 + 32.0
+            # temp_f = temp_c * 9.0 / 5.0 + 32.0
             return temp_c
-        
-
-    def _str_details(self):
-        return 'values=%s' % (str(self.value()))
 
     def value(self):
         """Read a temperature value in Celsius from the sensor
@@ -55,6 +55,9 @@ class AE_DS18B20(_AE_Peripheral_Base):
             return self._read_temp()
         except Exception:
             return None
+
+    def _str_details(self):
+        return 'values=%s' % (str(self.value()))
 
     def setup(self, **kwarg):
         # One throw away reads to get things going...
