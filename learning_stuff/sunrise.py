e180675d18ff3d73ca8ea7534377cc9209ba2d65
TJ <t_taiwo@live.co.uk>
2020-06-06 12:14:03 +0100

    Update: sunrise co2 sensor test script

--- learning_stuff/sunrise.py
+++ learning_stuff/sunrise.py
@@ -11,7 +11,7 @@ DEVICE = 0x68  # Default device I2C address
 bus = smbus.SMBus(1)  # Rev 2 Pi, Pi 2 & Pi 3 uses bus 1
 # Rev 1 Pi uses bus 0
 REG_CO2_DATA = 0x06
-
+sleep(1)
 
 def main(addr=DEVICE):
     for i in range(0, 11):
@@ -20,7 +20,7 @@ def main(addr=DEVICE):
         co2 = int.from_bytes(test, byteorder='big', signed=True)
         print(f'reference co2 is {co2}')
         bus.write_byte_data(addr, 0x06, 0)
-        sleep(0.01)
+        sleep(1/1000)
         data = bus.read_i2c_block_data(addr, 0x06, 2)
         co2_true = int.from_bytes(bytes(data), byteorder='big', signed=True)
         print(f' raw data is {data}')
